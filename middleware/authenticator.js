87b1c103ff2925048aa3198d02dae0103e94d066
aldairbalanzar <abalanzar7@gmail.com>
2020-10-06 00:07:12 -0400

    changed response for better error handling

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -9,13 +9,13 @@ module.exports = (req, res, next) => {
     jwt.verify(token, secret, (error, decodedToken) => {
       if (error) {
         console.log('No valid token.')
-        res.status(401).json({ message: "No valid token." });
+        res.status(401).json({ errorMessage: "No valid token." });
       } else {
         req.decodedToken = decodedToken;
         next();
       }})
   } else {
     console.log('Please provide credentials')
-    res.status(400).json({ message: "Please provide credentials." });
+    res.status(400).json({ errorMessage: "Please provide credentials." });
   }
 };
